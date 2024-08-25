e746efd0960150b21abf3b068deab1cc94b5ca3a
Thanatus <peetthanatus@gmail.com>
2024-08-25 15:23:54 +0700

    API Service with Spring Boot

---
+++ SE-Api/restaurant/src/main/java/ku/cs/restaurant/RestaurantApplication.java
@@ -0,0 +1,15 @@
+package ku.cs.restaurant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * @author 6510450429 Thanatus Suwan
+ */
+@SpringBootApplication
+public class RestaurantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestaurantApplication.class, args);
+	}
+
+}
